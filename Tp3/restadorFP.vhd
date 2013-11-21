f29a1e31ed4e07705f46ccccf9c832ff5f6ecab2
unknown <lucasp.simonelli@gmail.com>
2013-11-21 17:46:25 -0300

    Avances en el restador

--- Tp3/restadorFP.vhd
+++ Tp3/restadorFP.vhd
@@ -4,7 +4,7 @@ use IEEE.numeric_std.all;
 
 
 entity restadorFP is
-	generic(E:integer:=7; M:integer:=24);  -- m = mantisa, e=exponente
+	generic(E:integer:=8; M:integer:=23);  -- m = mantisa, e=exponente
 	port(
 		clk: in std_logic;
 		restando: in std_logic_vector(E+M downto 0);
@@ -16,13 +16,11 @@ entity restadorFP is
 end restadorFP;
 
 architecture beh of restadorFP is
-signal dummy: std_logic_vector(4 downto 0);
 signal e_a, e_b: integer;
 signal e_a_bits, e_b_bits: std_logic_vector(E-1 downto 0); --exponentes
 
 signal m_a, m_b: std_logic_vector(M-1 downto 0); --mantisas
 signal sig_a, sig_b: std_logic_vector(M downto 0); --significands
-signal s_a, s_b: std_logic; --signos
 signal p: std_logic_vector(M downto 0);
 signal temp_result: std_logic_vector(M downto 0);
 signal temp_result_no_desp: std_logic_vector(M downto 0);
@@ -41,85 +39,88 @@ signal loops: integer;
 begin
 	bias <= (2**E) - 1;
 
-	dummy <= "00001";
-
-	e_a_bits(E-1 downto 0) <= (others => '0');
-	e_b_bits(E-1 downto 0) <= (others => '0');
-	--e_a_bits(E-1 downto 0) <= restando(M+E-1 downto M);
-	--e_b_bits(E-1 downto 0) <= restador(M+E-1 downto M);
-	
-
 	-- paso 1 de las diapos (swap de operandos)
-	-- process(clk)
-	-- begin
-	-- 	e_a <= to_integer(unsigned(e_a_bits))-bias;
-	-- 	e_b <= to_integer(unsigned(e_b_bits))-bias;
+	 process(restando, restador)
+	 variable s_a, s_b: std_logic; --signos
+	 variable m_a_var, m_b_var: std_logic_vector(M-1 downto 0); --mantisas
+	 variable e_a_bits_var, e_b_bits_var: std_logic_vector(E-1 downto 0); --exponentes
+	 variable p_var: std_logic_vector(M downto 0);
+	 variable g_temp_var: std_logic;
+	 variable g_var, r_var: std_logic;
+	 variable s_var: std_logic_vector(M downto 0);
+	 begin
 
-	-- 	if (e_a < e_b) then
-	-- 		m_a <= restador(M-1 downto 0);
-	-- 		m_b <= restando(M-1 downto 0);
-	-- 		s_a <= restador(M+E);
-	-- 		s_b <= not restando(M+E); -- como es una resta basicamente invierto el signo del restador
-	-- 		e_b_bits <= restando(M+E-1 downto M);
-	-- 		e_a_bits <= restador(M+E-1 downto M);
-	-- 		e_a <= to_integer(unsigned(e_a_bits))-bias;
-	-- 		e_b <= to_integer(unsigned(e_b_bits))-bias;
-	-- 	else
-	-- 		m_a <= restando(M-1 downto 0);
-	-- 		m_b <= restador(M-1 downto 0);
-	-- 		s_a <= restando(M+E);
-	-- 		s_b <= not restador(M+E); -- como es una resta basicamente invierto el signo del restador
-	-- 		-- los exponentes no se tocan
-	-- 	end if;
+	 	if (restando(E+M-1 downto 0) < restador(E+M-1 downto 0)) then
+	 		m_a_var := restador(M-1 downto 0);
+	 		m_b_var := restando(M-1 downto 0);
+	 		s_a := restador(M+E);
+	 		s_b := restando(M+E);
+	 		-- s_b := not restando(M+E); -- como es una resta basicamente invierto el signo del restador
+	 		e_a_bits_var := restador(E+M-1 downto M);
+	 		e_b_bits_var := restando(E+M-1 downto M);
+	 	else
+	 		m_a_var := restando(M-1 downto 0);
+	 		m_b_var := restador(M-1 downto 0);
+	 		s_a := restando(M+E);
+	 		s_b := restador(M+E);
+	 		-- s_b := not restador(M+E); -- como es una resta basicamente invierto el signo del restador
+	 		e_a_bits_var := restando(E+M-1 downto M);
+	 		e_b_bits_var := restador(E+M-1 downto M);
+	 	end if;
 		
-	-- 	resta(M+E-1 downto M) <= e_a_bits; -- exp resultado -> exp operando A
-	-- end process;
-	-- --paso 2 de las diapos (complemento si b son != signos)
+	 	e_a <= to_integer(unsigned(e_a_bits_var))-bias;	
+	 	e_b <= to_integer(unsigned(e_b_bits_var))-bias;
 
-	-- 	if (not (s_a = s_b)) then
-	-- 		for I in 0 to M-1 loop
-	-- 			sig_b(I)<= not m_b(I);
-	-- 		end loop;		
-	-- 		sig_b(M)<='1';		
-	-- 	else 
-	-- 		sig_b(M-1 downto 0)<= m_b;
-	-- 		sig_b(M)<='1';
-	-- 	end if;
+	 	resta(E+M-1 downto M) <= e_a_bits; -- exp resultado -> exp operando A
+		--paso 2 de las diapos (complemento si b son != signos)
 
-	-- --paso 3
-	-- 	p(M) <= '1';
+	 	if (s_a /= s_b) then
+	 		m_b_var := not m_b_var;
+	 		m_b_var := std_logic_vector(unsigned(m_b_var) + 1);
+	 	end if;
+
+	 -- paso 3
+	 	-- p_var(M) := '1';
 		
-	-- 	for I in 0 to M-1 loop
-	-- 		p(I)<= sig_b(I);
-	-- 	end loop;
+	 	-- for I in 0 to M-1 loop
+	 	-- 	p_var(I) := sig_b(I);
+	 	-- end loop;
 		
-	-- 	if (e_a - e_b) = 1 then
-	-- 		g_temp <= p(0);
-	-- 		g <= p(0);
-	-- 	end if;
+	 	-- if (e_a - e_b) = 1 then
+	 	-- 	g_temp_var := p_var(0);
+	 	-- 	g_var := p_var(0);
+	 	-- end if;
 		
-	-- 	if (e_a - e_b) = 2 then
-	-- 		g_temp <= p(1);
-	-- 		g <= p(1);
-	-- 		r <= p(0);
-	-- 	end if;
+	 	-- if (e_a - e_b) = 2 then
+	 	-- 	g_temp_var := p_var(1);
+	 	-- 	g_var := p_var(1);
+	 	-- 	r_var := p_var(0);
+	 	-- end if;
 		
-	-- 	if (e_a - e_b) > 2 then
-	-- 		g_temp <= p(e_a - e_b - 1);
-	-- 		g <= p(e_a - e_b - 1);
-	-- 		r <= p(e_a - e_b - 2);
-	-- 		s(e_a - e_b downto 0) <= p(e_a - e_b downto 0);
-	-- 	end if;
+	 	-- if (e_a - e_b) > 2 then
+	 	-- 	g_temp_var := p_var(e_a - e_b - 1);
+	 	-- 	g_var := p(e_a - e_b - 1);
+	 	-- 	r_var := p(e_a - e_b - 2);
+	 	-- 	s_var(e_a - e_b downto 0) := p_var(e_a - e_b downto 0);
+	 	-- end if;
 		
-	-- 	if (not (s_a = s_b)) then
-	-- 		for I in M to (e_a - e_b) loop
-	-- 			p(I)<= '1';
-	-- 		end loop;
-	-- 	else
-	-- 		for I in M to (e_a - e_b) loop
-	-- 			p(I)<= '0';
-	-- 		end loop;
-	-- 	end if;
+	 	-- if (not (s_a = s_b)) then
+	 	-- 	for I in M to (e_a - e_b) loop
+	 	-- 		p_var(I) := '1';
+	 	-- 	end loop;
+	 	-- else
+	 	-- 	for I in M to (e_a - e_b) loop
+	 	-- 		p_var(I) := '0';
+	 	-- 	end loop;
+	 	-- end if;
+
+	 	m_a <= m_a_var;
+	 	m_b <= m_b_var;
+	 	p <= p_var;	
+	 	r <= r_var;
+	 	g <= g_var;
+	 	s <= s_var;
+	end process;
 
 	-- --paso 4
 	-- 	temp_result(M downto 0) <= std_logic_vector(signed(sig_b) + signed(p));
@@ -187,4 +188,4 @@ begin
 	-- 		resta(M + E) <= s_a;
 	-- 	end if;
 	-- end process;
-end;
+end architecture;
